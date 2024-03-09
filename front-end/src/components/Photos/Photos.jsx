c3d70800ce98e62d6008f26eb8e0277cd48391d9
Kris1027 <obarzanek.work@gmail.com>
2024-03-09 20:44:07 +0100

    build product details components

---
+++ front-end/src/components/Photos/Photos.jsx
@@ -0,0 +1,25 @@
+import styles from "./Photos.module.css";
+import { FlexContainer } from "../FlexContainer/FlexContainer";
+import { useState } from "react";
+
+export function Photos({ product }) {
+  const [currentPhoto, setCurrentPhoto] = useState(product.photos[0]);
+
+  return (
+    <FlexContainer>
+      <div className={styles.thumbnails}>
+        {product.photos.map((photo) => {
+          return (
+            <img
+              className={currentPhoto === photo ? styles.active : ""}
+              key={photo}
+              src={photo}
+              onClick={() => setCurrentPhoto(photo)}
+            />
+          );
+        })}
+      </div>
+      <img src={currentPhoto} className={styles.mainPhoto} />
+    </FlexContainer>
+  );
+}
