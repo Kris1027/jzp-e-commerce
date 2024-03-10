b19420c4e2a6a6a38a2875fe5c9472ab0a4d8051
Kris1027 <obarzanek.work@gmail.com>
2024-03-10 11:00:01 +0100

    connect products list with backend

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -9,6 +9,8 @@ import { Favourites } from "./views/Favourites/Favourites";
 import { Layout } from "./components/Layout/Layout";
 import { MainPage } from "./views/MainPage/MainPage";
 import { mainPageLoader } from "./api/mainPageLoader";
+import { ProductsList } from "./views/ProductsList/ProductsList";
+import { productListLoader } from "./api/productListLoader";
 
 const router = createBrowserRouter([
   {
@@ -27,6 +29,11 @@ const router = createBrowserRouter([
         path: ":gender?",
         element: <MainPage />,
         loader: mainPageLoader,
+      },
+      {
+        path: "/:gender/:category/:subcategory?",
+        element: <ProductsList />,
+        loader: productListLoader,
       },
     ],
   },
