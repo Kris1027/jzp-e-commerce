ffd17f192f13375d9b07389cacf49fc0ff8f1dbc
Kris1027 <obarzanek.work@gmail.com>
2024-03-10 10:00:56 +0100

    connecting main page with backend and react router

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -7,6 +7,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Cart } from "./views/Cart/Cart";
 import { Favourites } from "./views/Favourites/Favourites";
 import { Layout } from "./components/Layout/Layout";
+import { MainPage } from "./views/MainPage/MainPage";
+import { mainPageLoader } from "./api/mainPageLoader";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +22,11 @@ const router = createBrowserRouter([
       {
         path: "/ulubione",
         element: <Favourites />,
+      },
+      {
+        path: ":gender?",
+        element: <MainPage />,
+        loader: mainPageLoader,
       },
     ],
   },
